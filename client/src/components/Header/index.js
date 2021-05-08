b91e126e4bd6b5006e666eb242e9b750cbb003d0
Farouk <faroukkisuule55@gmail.com>
2021-05-08 11:16:46 -0400

    Updated navbar

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,16 +2,113 @@ import React from "react";
 import "./Header.css";
 
 function Header() {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light">
-        <div className="container">
-          <a className="navbar-brand" href="." id="logo">PurrChase</a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01"
-            aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
+  return (
+    <nav className="navbar navbar-icon-top navbar-expand-lg navbar-dark bg-dark">
+      <div className="container align-middle">
+        <a className="navbar-brand" href="/">
+          <h2 classNameName="py-3"> PurrChase </h2>
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="/navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse " id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="/">
+                {/* <i className="fa fa-home"></i> */}
+                <h5> Home </h5>
+                <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" href="/">
+                {/* <i className="fa fa-envelope-o">
+                  <span className="badge badge-danger">11</span>
+                </i> */}
+                <h5>Categories</h5>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" href="/">
+                {/* <i className="fa fa-envelope-o">
+                  <span className="badge badge-warning">11</span>
+                </i> */}
+                <h5>Stats</h5>
+              </a>
+            </li>
+            {/* <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="/"
+                id="navbarDropdown"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                <i className="fa fa-envelope-o">
+                  <span className="badge badge-primary">11</span>
+                </i>
+                Dropdown
+              </a>
+              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a className="dropdown-item" href="/">
+                  Action
+                </a>
+                <a className="dropdown-item" href="/">
+                  Another action
+                </a>
+                <div className="dropdown-divider"></div>
+                <a className="dropdown-item" href="/">
+                  Something else here
+                </a>
+              </div>
+            </li> */}
+          </ul>
+          {/* <ul className="navbar-nav ">
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                <i className="fa fa-bell">
+                  <span className="badge badge-info">11</span>
+                </i>
+                Test
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                <i className="fa fa-globe">
+                  <span className="badge badge-success">11</span>
+                </i>
+                Test
+              </a>
+            </li>
+          </ul> */}
+          <form className="form-inline my-2 my-lg-0">
+            <input
+              className="form-control mr-sm-2"
+              type="text"
+              placeholder="Search"
+              aria-label="Search"
+            ></input>
+            <button
+              className="btn btn-outline-success my-2 my-sm-0"
+              type="submit"
+            >
+              <h5>Search</h5>
+            </button>
+          </form>
         </div>
-      </nav>
-    );
+      </div>
+    </nav>
+  );
 }
 export default Header;
