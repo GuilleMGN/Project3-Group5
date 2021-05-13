a55e793253b04e31354a7cfd05fa5f03d0356966
Matthew Guillen <matthewguillen777@gmail.com>
2021-05-12 21:49:08 -0400

    readd models and import fix product

--- client/src/utils/ProductAPI.js
+++ client/src/utils/ProductAPI.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const ProductAPI = {
     create: function (productObject) {
-        return axios.post("/api/products/add", productObject); 
+        return axios.post("/api/product/add", productObject); 
     },
     getProduct: function () {
         return axios.get("/api/product");
